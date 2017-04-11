2d57782a0291a6e383121a78352e1fba3590b721
Guru Prasath G <theemailidofthemaster@gmail.com>
2017-04-11 23:24:00 +0530

    populate map with xml data from sql server

--- map.js
+++ map.js
@@ -6,4 +6,38 @@ function init() {
         center: { lat: -34.397, lng: 150.644 },
         zoom: 8
     });
+
+
+    downloadUrl('http://localhost/gps/query.php', function (data) {
+        var xml = data.responseXML;
+        var markers = xml.documentElement.getElementsByTagName('marker');
+
+        Array.prototype.forEach.call(markers, function (markerElem) {
+            var id = markerElem.getAttribute('id');
+            var point = new google.maps.LatLng(
+                parseFloat(markerElem.getAttribute('lat')),
+                parseFloat(markerElem.getAttribute('lng')));
+
+            var marker = new google.maps.Marker({
+                map: map,
+                position: point
+            });
+        });
+    });
 }
+
+function downloadUrl(url, callback) {
+    var request = window.ActiveXObject ? new ActiveXObject('Microsoft.XMLHTTP') : new XMLHttpRequest;
+
+    request.onreadystatechange = function () {
+        if (request.readyState == 4) {
+            request.onreadystatechange = doNothing;
+            callback(request, request.status);
+        }
+    };
+
+    request.open('GET', url, true);
+    request.send(null);
+}
+
+function doNothing() {}
