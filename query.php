941524b3064af0bf90a6493a82916838b18a23cd
Guru Prasath G <theemailidofthemaster@gmail.com>
2017-04-11 23:06:09 +0530

    echo is used for xml generation, to reduce dependancies.

--- query.php
+++ query.php
@@ -1,11 +1,6 @@
 <?php
 require("creds.php");
 
-// Start XML file, create parent node
-$doc = domxml_new_doc("1.0");
-$node = $doc->create_element("markers");
-$parnode = $doc->append_child($node);
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -15,26 +10,28 @@ if ($conn->connect_error) {
 
 // Select all the rows in the `test` table
 $sql = "SELECT * FROM test WHERE 1";
-$result = mysql_query($sql);
+$result = mysqli_query($conn,$sql);
 if (!$result) {
-  die('Invalid query: ' . mysql_error());
+  die('Invalid query: ' . mysqli_error($conn));
 }
 
 header("Content-type: text/xml");
 
-// Iterate through the rows, adding XML nodes for each
-while ($row = @mysql_fetch_assoc($result)){
-  // Add to XML document node
-  $node = $doc->create_element("marker");
-  $newnode = $parnode->append_child($node);
+// Start XML file, echo parent node
+echo '<markers>';
 
-  $newnode->set_attribute("id", $row['id']);
-  $newnode->set_attribute("lat", $row['lat']);
-  $newnode->set_attribute("lng", $row['lng']);
+// Iterate through the rows, printing XML nodes for each
+while ($row = @mysqli_fetch_assoc($result)){
+  // Add to XML document node
+  echo '<marker ';
+  echo 'id="' . $row['id'] . '" ';
+  echo 'lat="' . $row['lat'] . '" ';
+  echo 'lng="' . $row['lon'] . '" ';
+  echo '/>';
 }
 
-$xmlfile = $doc->dump_mem();
-echo $xmlfile;
+// End XML file
+echo '</markers>';
 
 $conn->close();
 ?>
