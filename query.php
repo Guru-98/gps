084cc8f936aebbc0d1e52e1ccbe767835dc2c52f
Guru Prasath G <theemailidofthemaster@gmail.com>
2017-04-11 22:32:17 +0530

    * changed query.php to add.php * placed creds in seperate file * made query.php to serve loactions as xml from sql server to populate the map

--- query.php
+++ query.php
@@ -1,8 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "locationDB";
+require("creds.php");
+
+// Start XML file, create parent node
+$doc = domxml_new_doc("1.0");
+$node = $doc->create_element("markers");
+$parnode = $doc->append_child($node);
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,13 +13,28 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "INSERT INTO test (id, lat, lon) VALUES ('" . $_GET["id"] . "', '" . $_GET["lat"] . "', '" . $_GET["lon"] . "')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+// Select all the rows in the `test` table
+$sql = "SELECT * FROM test WHERE 1";
+$result = mysql_query($sql);
+if (!$result) {
+  die('Invalid query: ' . mysql_error());
 }
+
+header("Content-type: text/xml");
+
+// Iterate through the rows, adding XML nodes for each
+while ($row = @mysql_fetch_assoc($result)){
+  // Add to XML document node
+  $node = $doc->create_element("marker");
+  $newnode = $parnode->append_child($node);
+
+  $newnode->set_attribute("id", $row['id']);
+  $newnode->set_attribute("lat", $row['lat']);
+  $newnode->set_attribute("lng", $row['lng']);
+}
+
+$xmlfile = $doc->dump_mem();
+echo $xmlfile;
 
 $conn->close();
 ?>
